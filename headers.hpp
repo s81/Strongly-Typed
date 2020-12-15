d4dfeefbc4a2cccccbbf960efc3d68e9eb5af90b
Samer William <samer.w.alhadaddin@gmail.om>
2020-12-15 16:43:07 +0200

    Ranges in?

--- headers.hpp
+++ headers.hpp
@@ -1,7 +1,3 @@
 #include <iostream>
-#include <vector> 
 #include <algorithm>
-#include <string>
 #include <ranges>
-#include <chrono>
-
--- ney.cc
+++ ney.cc
@@ -3,7 +3,8 @@
 
 template <typename named_type, typename tag_type, typename ratio>
 struct unit{
-    explicit unit(named_type value_) :value(std::move(value_)) {}; 
+    explicit unit(named_type value_) 
+        :value(std::move(value_)) {}; 
     named_type value{};
 };
 
@@ -12,8 +13,8 @@ using decimal = long double;
 using dollar = unit<double, struct dollar_, std::ratio<100,1>>;
 
 
-cent    operator "" _cents(uint64_t value_) { return cent(value_); }
-dollar  operator"" _dollars(decimal value_) { return dollar(value_); }
+cent   operator "" _cents(uint64_t value_) { return cent(value_); }
+dollar operator"" _dollars(decimal value_) { return dollar(value_); }
 dollar operator+ (dollar ds, cent cs) { return dollar(ds.value + (0.01 * cs.value));}
 dollar operator= (cent cs) { return dollar(0.01 * cs.value);}
 
